7aa6c3984ded480b32f6f95f58e5c849ca4f0ed9
ianmax32 <masagaian32@gmail.com>
2022-12-10 14:24:58 -0800

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # creating a file in /tmp using puppet
 
 file {'/tmp/school':
-    ensure => file,
-    owner    => 'www-data',
-    group    => 'www-data',
-    content  => 'I love Puppet',
-    mode     => '0744',
-    path     => '/tmp/school'
+    ensure  => file,
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+    mode    => '0744',
+    path    => '/tmp/school'
 }
