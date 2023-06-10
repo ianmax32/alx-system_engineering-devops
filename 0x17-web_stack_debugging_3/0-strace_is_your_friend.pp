b1dfcfc3cb0e4a26d15275bf871ea9c5fa18b4a6
ianmax32 <masagaian32@gmail.com>
2023-06-09 21:00:10 -0700

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # this is to find out why apache is returning a 500 error using strace
 exec {
-   'phpp':
-   command => '/bin/sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php'
+	'phpp':
+	command => '/bin/sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php'
 }
